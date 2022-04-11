113a0fbf1af6debb3b2d548f60506e3138afb54f
pierluigi <pierluigibaiano@gmail.com>
2022-04-11 13:54:41 +0200

    install react router dom v6 and create Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import SearchRepos from "./components/SearchRepos";
+import UserInfo from "./components/UserInfo";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <SearchRepos />
+      <h1>Github repos finder</h1>
+      <Routes>
+        <Route path="/" element={<SearchRepos />} />
+        <Route path="/userinfo" element={<UserInfo />} />
+      </Routes>
     </div>
   );
 }
