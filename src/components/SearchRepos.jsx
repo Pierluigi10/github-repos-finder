e248f2cf04de5ed83f79514cf74ba0ec1c20d6a6
pierluigi <pierluigibaiano@gmail.com>
2022-04-11 16:59:50 +0200

    change api: sort api by repoName

--- src/components/SearchRepos.jsx
+++ src/components/SearchRepos.jsx
@@ -2,33 +2,76 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function SearchRepos() {
   const [reposInfo, setReposInfo] = useState([]);
-  const [searchRepos, setSearchRepos] = useState("");
+  // const [searchRepos, setSearchRepos] = useState("");
+
+  const [repoName, setRepoName] = useState("");
+  // const [repositories, setRepositories] = useState([]);
 
   const navigate = useNavigate();
 
-  const handleButton = async () => {
-    try {
-      const response = await fetch(
-        `https://api.github.com/users/${searchRepos}/repos`
-      );
-      const data = await response.json();
-      setReposInfo([...data]);
-      console.log([...data]);
-    } catch (err) {
-      alert("no repos found, try again");
-      setSearchRepos("");
-    }
-  };
+  // const handleButton = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.github.com/users/${searchRepos}/repos`
+  //     );
+  //     const data = await response.json();
+  //     setReposInfo([...data]);
+  //     console.log([...data]);
+  //   } catch (err) {
+  //     alert("no repos found, try again");
+  //     setSearchRepos("");
+  //   }
+  // };
 
+  // 
+  //   const handleButton = async () => {
+  //     const response = await fetch(`https://api.github.com/users/${username}/repos`);
+  //     const data = await response.json();
+  //     // console.log(data);
+  
+  //     // const languages= await fetch(data.repos_url);
+  //     // const repos = await repositories.json();
+  //     // console.log(repos);
+
+  //     setReposInfo([...data])
+
+
+  // };
+
+  
+
+  const handleButton = async () => {
+    const response = await fetch(`https://api.github.com/search/repositories?q=${repoName}&per_page=6`);
+    const data = await response.json();
+    console.log(data.items);
+
+    // const languages= await fetch(data.repos_url);
+    // const repos = await repositories.json();
+    // console.log(repos);
+
+    setReposInfo([...data.items])
+
+
+};
   return (
     <div className="App">
-      <input
+      {/* <input
         type="text"
         value={searchRepos}
         onChange={(e) => {
           setSearchRepos(e.target.value);
         }}
-      />
+      /> */}
+
+<input
+              className="prompt"
+              placeholder="search your github.."
+              type="text"
+              value={repoName}
+              onChange={(e) => {
+                setRepoName(e.target.value);
+              }}
+            />
       <button type="submit" onClick={handleButton}>
         search
       </button>
@@ -51,7 +94,10 @@ function SearchRepos() {
             >
               {item.name}
             </h2>
-            <p>{item.language}</p>
+            <p>{item.description}</p>
+            <p>{item.language}</p> 
+            <p>{item.name}</p>
+            <p>{item.stargazers_count}</p>
           </div>
         );
       })}
