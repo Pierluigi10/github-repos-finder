9be4f375f0e9b431e1bb93c3fef77b13f08db243
pierluigi <pierluigibaiano@gmail.com>
2022-04-12 08:56:14 +0200

    add easy style to SearchRepos and permit to click on the card to receive the UserInfo

--- src/components/SearchRepos.jsx
+++ src/components/SearchRepos.jsx
@@ -1,3 +1,4 @@
+import "../style/searchRepos.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import getRating from "../rating.js";
@@ -6,40 +7,9 @@ import { FaRegEye } from "react-icons/fa";
 
 function SearchRepos() {
   const [reposInfo, setReposInfo] = useState([]);
-  // const [searchRepos, setSearchRepos] = useState("");
-
   const [repoName, setRepoName] = useState("");
-  // const [repositories, setRepositories] = useState([]);
 
   const navigate = useNavigate();
-
-  // const handleButton = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://api.github.com/users/${searchRepos}/repos`
-  //     );
-  //     const data = await response.json();
-  //     setReposInfo([...data]);
-  //     console.log([...data]);
-  //   } catch (err) {
-  //     alert("no repos found, try again");
-  //     setSearchRepos("");
-  //   }
-  // };
-
-  //
-  //   const handleButton = async () => {
-  //     const response = await fetch(`https://api.github.com/users/${username}/repos`);
-  //     const data = await response.json();
-  //     // console.log(data);
-
-  //     // const languages= await fetch(data.repos_url);
-  //     // const repos = await repositories.json();
-  //     // console.log(repos);
-
-  //     setReposInfo([...data])
-
-  // };
 
   const handleButton = async () => {
     const response = await fetch(
@@ -48,65 +18,63 @@ function SearchRepos() {
     const data = await response.json();
     console.log(data.items);
 
-    // const languages= await fetch(data.repos_url);
-    // const repos = await repositories.json();
-    // console.log(repos);
-
     setReposInfo([...data.items]);
   };
   return (
-    <div className="App">
-      {/* <input
-        type="text"
-        value={searchRepos}
-        onChange={(e) => {
-          setSearchRepos(e.target.value);
-        }}
-      /> */}
-
+    <div className="SearchRepos">
       <input
-        className="prompt"
-        placeholder="search your github.."
+        className="searchInput"
+        placeholder="search the repos"
         type="text"
         value={repoName}
         onChange={(e) => {
           setRepoName(e.target.value);
         }}
       />
-      <button type="submit" onClick={handleButton}>
+      <button className="btnSearch" type="submit" onClick={handleButton}>
         search
       </button>
-
-      {reposInfo.map((item) => {
-        return (
-          <div key={item.id}>
-            <img
-              src={item.owner.avatar_url}
-              alt={item.owner.type}
-              width="50px"
-              height="50px"
-            />
-            <h2
+      <div className="repos">
+        {reposInfo.map((item) => {
+          return (
+            <div
+              className="card"
               onClick={() => {
                 navigate(`/userinfo`, {
                   state: item,
                 });
               }}
+              key={item.id}
             >
-              {item.name}
-            </h2>
-            <p>{item.description}</p>
-            <p>{item.language}</p>
-            <p>{item.name}</p>
-            <p>{item.stargazers_count}</p>
-            {/* <p>{item.watchers_count}</p> */}
-            <p>
-              <FaRegEye /> {kConverter(item.watchers_count)}
-            </p>
-            <p>{getRating(item.stargazers_count)}</p>
-          </div>
-        );
-      })}
+              <img
+                className="avatar"
+                src={item.owner.avatar_url}
+                alt={item.owner.type}
+                width="50px"
+                height="50px"
+              />
+              <h2
+                onClick={() => {
+                  navigate(`/userinfo`, {
+                    state: item,
+                  });
+                }}
+              >
+                {item.name}
+              </h2>
+              <p>{item.description}</p>
+              <p>{item.language}</p>
+              <p>{item.name}</p>
+              <p>{item.stargazers_count}</p>
+              {/* <p>{item.watchers_count}</p> */}
+              <p>
+                <FaRegEye /> {kConverter(item.watchers_count)}
+              </p>
+              <p>{getRating(item.stargazers_count)}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
