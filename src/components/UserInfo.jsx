b57027abdcc820fbee090efeb6a03ee8cf5ac406
pierluigi <pierluigibaiano@gmail.com>
2022-04-12 12:33:51 +0200

    add following people

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import "../style/userInfo.css";
 
 function UserInfo() {
   const navigate = useNavigate();
@@ -8,6 +9,7 @@ function UserInfo() {
   const infos = location.state;
   const [userBio, setUserBio] = useState("");
   const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -23,29 +25,53 @@ function UserInfo() {
       const followersList = await fetchFollowers.json();
       // console.log(followersList.length)
       setFollowers(followersList.length);
+
+      const fetchFollowing = await fetch(
+        `https://api.github.com/users/${infos.owner.login}/following`
+      );
+      const followingList = await fetchFollowing.json();
+      // console.log(followersList.length)
+      setFollowing(followingList.length);
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   console.log(infos);
   return (
-    <div>
-      <h2>UserInfo</h2>
-      <button onClick={() => navigate("/")}>Home</button>
+    <div className="UserInfo">
+      <h2>Githuber Details</h2>
+
       <div className="info">
         <img
+          className="avatar"
           src={infos.owner.avatar_url}
           alt={infos.owner.type}
           width="50px"
           height="50px"
         />
-        <p>{infos.owner.login}</p>
+        <a href={infos.owner.html_url} target="_blank" rel="noreferrer">
+          {infos.owner.login}
+        </a>
+        <div className="follwInfo">
+        {followers > 0 && (
+          <p>
+            <b>{followers}</b> followers
+          </p>
+        )}
+        {following > 0 && (
+          <p>
+            <b>{following}</b> Following
+          </p>
+        )}</div>
         <p>{userBio}</p>
-        {followers > 0 && <p>Followers: {followers}</p>}
 
         {/* <p>{console.log(infos.description)}</p>
         <p>{console.log(infos.owner.followers_url)}</p> */}
       </div>
+
+      <button className="btnToHome" onClick={() => navigate("/")}>
+        Home
+      </button>
     </div>
   );
 }
